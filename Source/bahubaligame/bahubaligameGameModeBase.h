3869fdd8c99f9a6e000b52956e1a2d2452c47e60
anudeep0007 <anudeep950@gmail.com>
2019-12-27 11:31:00 +0530

    first commit
    first commit ..no fancy stuff

---
+++ Source/bahubaligame/bahubaligameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "bahubaligameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BAHUBALIGAME_API AbahubaligameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
